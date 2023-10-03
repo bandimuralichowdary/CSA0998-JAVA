f207fe6f592b364473e5cdbcfeba379035ed111f
Bandi Murali Chowdary <97547174+bandimuralichowdary@users.noreply.github.com>
2023-10-03 22:22:35 +0530

    Create Username.java

---
+++ Username.java
@@ -0,0 +1,21 @@
+// Username is valid or not
+
+import java.util.*;
+class Username{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        String Username1, Username2;
+        System.out.print("Enter the Username1:");
+        Username1=s.nextLine();
+        System.out.print("Enter the Username2:");
+        Username2=s.nextLine();
+        if(Username1.equals(Username2))
+        {
+            System.out.print("Username is Valid");
+        }
+        else{
+            System.out.print("Username is Invalid");
+        }
+    }
+}
