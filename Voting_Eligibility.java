a295bd01301d15bcf2e6993f8cd246cdd3b60af3
Bandi Murali Chowdary <97547174+bandimuralichowdary@users.noreply.github.com>
2023-10-03 12:58:39 +0530

    Update Voting_Eligibility.java

--- Voting_Eligibility.java
+++ Voting_Eligibility.java
@@ -5,21 +5,21 @@ class HelloWorld {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int age,elg;
-        System.out.println("Enter your birth year:");
+        System.out.println("Enter your AGE:");
         age=s.nextInt();
         elg=18-age;
-        if(age<=0)
-        {
-            System.out.println("Enter a valid AGE\n");
-        }
-        if(age<18)
+        if(age>0 && age<18)
         {
             System.out.println("You are not eligible for voting");
             System.out.printf("You will be eligible after %d years\n",elg);
         }
-        else
+        else if(age>=18)
         {
             System.out.println("You are eligible for Voting\n");
+        }
+        else
+        {
+            System.out.println("Enter a valid AGE\n");
         }
     }
 }
