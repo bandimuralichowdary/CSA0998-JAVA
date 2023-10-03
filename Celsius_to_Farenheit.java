050a80662a8adae827adc0f2295a410d34087beb
Bandi Murali Chowdary <97547174+bandimuralichowdary@users.noreply.github.com>
2023-10-03 12:24:07 +0530

    Update Celsius_to_Farenheit.java

--- Celsius_to_Farenheit.java
+++ Celsius_to_Farenheit.java
@@ -6,8 +6,9 @@ class HelloWorld {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int c, fin;
+        System.out.println("Enter the Celsius Temperature: ");
         c = s.nextInt();
         fin = (c * 9 / 5) + 32;
-        System.out.println("Output = " + fin);
+        System.out.println("The equivalent Farenheit value for given is = " + fin);
     }
 }
